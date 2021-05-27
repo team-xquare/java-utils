0c61c694b6f33bbc01b58200df25f6b8f61eca2b
smoothbear <kjbin0420@naver.com>
2021-05-27 17:26:20 +0900

    :bug: :: (#2) gpg import path 수정

--- .github/workflows/commands/deploy.sh
+++ .github/workflows/commands/deploy.sh
@@ -18,7 +18,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
 gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
     --output gradle.properties .github/workflows/commands/gpg/gradle.properties.gpg
 
-gpg --fast-import --no-tty --batch --yes .github/workflows/commands/gpg/smoothbear04-sign.asc
+gpg --fast-import --no-tty --batch --yes smoothbear04-sign.asc
 
 echo "📦 Publishing..."
 
